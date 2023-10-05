af2e65f8d757ab0115c9d5bf9b0f11db0e9c327f
SkelisolVision <krickett86@gmail.com>
2023-10-05 17:30:54 -0500

    Testing commit. 10052023 jdt

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,7 @@ function App() {
 
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
+      
       <div className="navbar">
         <ul>
           <li>Logo</li>
@@ -31,17 +24,7 @@ function App() {
         <h1>JARVIS TAYLOR</h1>
         <h2>Front End Web Developer</h2>
       </header>
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      
     </>
   )
 }
