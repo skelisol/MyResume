91d77503c4586ad46dad164e893645d797e09e37
SkelisolVision <krickett86@gmail.com>
2023-10-17 18:11:43 -0500

    Testing updates with Hero section

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
                 <h3>Hi, I'm</h3>        
                 <h1>JARVIS TAYLOR</h1>
                 <h2>Software/Web Developer</h2>
-                <p>Let's show the world that magic still exists!</p>
+                <p>Let's show the world that magic still exists!!</p>
                 <button> Contact Me </button>
             </main>
         </div>
